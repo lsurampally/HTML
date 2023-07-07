80ed82142303f8f837a46136d76b54309af36e61
lsurampally <lekhana.surampally@gmail.com>
2023-07-06 23:56:32 -0400

    Initial commit to local

---
+++ ifstatement.js
@@ -0,0 +1,8 @@
+let x = 20 * 3456;
+let y = 30 * 2345;
+
+if (x = y) 
+  { console.log("both are same")} 
+    else 
+  {console.log("They are not same")} ;
+
---
+++ index.js
@@ -0,0 +1,7 @@
+let a = 2;
+let b = 3;
+let c;
+
+c = a * b;
+
+console.log(c) ;
